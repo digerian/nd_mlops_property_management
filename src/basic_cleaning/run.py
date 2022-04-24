d128f2327c66b269c6da3787272d54938b03342d
David Venzke <digerian@protonmail.com>
2022-04-23 19:03:14 -0500

    completed step 2

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,8 @@ Performs basic cleaning on the data and save the results in Weights & Biases
 import argparse
 import logging
 import wandb
+import pandas as pd
+import os
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -29,19 +31,28 @@ def go(args):
     artifact = run.use_artifact(args.input_artifact)
     artifact_path = artifact.file()
 
-    df = pd.read_parquet(artifact_path)
+    df = pd.read_csv(artifact_path, low_memory=False)
+
 
     # Drop the duplicates
     logger.info("Dropping duplicates")
     df = df.drop_duplicates().reset_index(drop=True)
 
-    filename = "cleaned_data.csv"
-    df.to_csv(filename)
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
 
     artifact = wandb.Artifact(
-        name=args.artifact_name,
-        type=args.artifact_type,
-        description=args.artifact_description,
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
     )
     artifact.add_file(filename)
 
@@ -63,102 +74,38 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--artifact_name", type=str, help="Name for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_type", type=str, help="Type for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_description",
+        "--output_artifact",
         type=str,
-        help="Description for the artifact",
-        required=True,
-    )
-
-    args = parser.parse_args()
-
-    go(args)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def go(args):
-
-    run = wandb.init(job_type="process_data")
-
-    logger.info("Downloading artifact")
-    artifact = run.use_artifact(args.input_artifact)
-    artifact_path = artifact.file()
-
-    df = pd.read_parquet(artifact_path)
-
-    # Drop the duplicates
-    logger.info("Dropping duplicates")
-    df = df.drop_duplicates().reset_index(drop=True)
-
-    # A minimal feature engineering step: a new feature
-    logger.info("Feature engineering")
-    df['title'].fillna(value='', inplace=True)
-    df['song_name'].fillna(value='', inplace=True)
-    df['text_feature'] = df['title'] + ' ' + df['song_name']
-
-    filename = "processed_data.csv"
-    df.to_csv(filename)
-
-    artifact = wandb.Artifact(
-        name=args.artifact_name,
-        type=args.artifact_type,
-        description=args.artifact_description,
-    )
-    artifact.add_file(filename)
-
-    logger.info("Logging artifact")
-    run.log_artifact(artifact)
-
-    os.remove(filename)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Preprocess a dataset",
-        fromfile_prefix_chars="@",
+        help="Name for the output artifact",
+        required=True
     )
 
     parser.add_argument(
-        "--input_artifact",
+        "--output_description",
         type=str,
-        help="Fully-qualified name for the input artifact",
-        required=True,
+        help="Description for the output artifact",
+        required=True
     )
 
     parser.add_argument(
-        "--artifact_name", type=str, help="Name for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_type", type=str, help="Type for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_description",
+        "--output_type",
         type=str,
-        help="Description for the artifact",
-        required=True,
+        help="Type of output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="minimum price for apartment",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="maximum price for apartment",
+        required=True
     )
 
     args = parser.parse_args()
